e4d326a4f9e5621fded142513702ee9f7f97b0c3
Giotek27 <serleonard123@gmail.com>
2021-10-08 22:18:54 -0500

    se corrigió

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let bodyParser=require("body-parser");
 let cors=require("cors");
 let express=require("express");
 require("dotenv").config();
-var auth = require("./routes/auth");
+
 
 mongoose.Promise=global.Promise;
 mongoose.connect(
@@ -32,7 +32,7 @@ app.use((req,res,next)=>{
     next();
 });
 
-app.use("/api/auth", auth);
+
 
 
 const vendorapi=require("./routes/vendedoroute");
